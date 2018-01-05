fe005940417a3fd6ed364196c30ab9e8063198c7
teddimagg <teddi@menn.is>
2018-01-05 21:31:35 +0000

    sorting history

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ let icelandic = read.sync('icelandic.txt').toString().split("\n");
 const uniq = a => [...new Set(a)];
 const remove = (array, element) => array.splice(array.indexOf(element), 1);
 
+let history = {};
+history['smegma'] = 'smegma';
 // Solver
 const solver = (letters, length) => {
-	letters = Array.from(letters.toLowerCase());
-	const combinations = getCombinations(letters, length);
+	// Fetch from history if existing
+	letters = letters.split('').sort().join('');
+	if(history[letters] != null && history[letters][length.toString()]) return history[letters][length.toString()];
+	console.log('not in history');
 
+	const combinations = getCombinations(Array.from(letters.toLowerCase()), length);
 	let results = [];
+
 	for(word of icelandic){
 		if(word.length != length) continue;
 		word = word.toLowerCase();
@@ -26,10 +32,16 @@ const solver = (letters, length) => {
 			valid && results.push(word);
 		}
 	}
+
+	// Adding to history
+	if(!history[letters]) history[letters] = {};
+	history[letters][length.toString()] = uniq(results);
+
 	return uniq(results);
 }
 
-// console.log( solver("skjár", 3) || "Engin niðurstaða" );
+console.log( solver("skjár", 3) || "Engin niðurstaða" );
+console.log( solver("sjárk", 3) || "Engin niðurstaða" );
 
 // **************************************************************
 	// SERVER
